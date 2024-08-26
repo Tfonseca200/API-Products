6f76625539327e0a0eeb15ec2eda49151cf50d31
Thiago <thiagofonsecaclaudino@gmail.com>
2024-08-26 03:01:01 -0300

    feat: adicionando product controller com as operações CRUD

--- src/main/java/com/example/springboot/controllers/ProductController.java
+++ src/main/java/com/example/springboot/controllers/ProductController.java
@@ -1,10 +1,18 @@
 package com.example.springboot.controllers;
 
+import com.example.springboot.dtos.ProductRecordDto;
 import com.example.springboot.models.ProductModel;
 import com.example.springboot.repositories.ProductRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 public class ProductController {
@@ -12,5 +20,61 @@ public class ProductController {
     @Autowired
     private ProductRepository productRepository;
 
+    /*Método POST (Create)
+    @RequestBody é pra receber o corpo da requisição
+    @Valid é necessario pra validar os atributos da classe do record product (ProductRecordDto) */
 
+    @PostMapping("/products")
+    public ResponseEntity<ProductModel> saveProduct(@RequestBody @Valid ProductRecordDto productRecordDto){
+        ProductModel productModel = new ProductModel();
+
+        // metódo que faz a realização da conversão de um objeto record pra um outro objeto (<objetoQueVaiSerConvertido>, <objetoDaConversão)
+        BeanUtils.copyProperties(productRecordDto, productModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(productRepository.save(productModel));
+
+        /* podemos implementa várias melhorias, por exemplo: customização de mensagem, customização de erro,
+        inserir um custom validation , exception handler e entre outros
+        */
+    }
+
+    @GetMapping("/products")
+    public ResponseEntity<List<ProductModel>> getAllProducts(){
+        return ResponseEntity.status(HttpStatus.OK).body(productRepository.findAll());
+    }
+
+    @GetMapping("/products/{id}")
+    public ResponseEntity<Object> getOneProduct(@PathVariable(value = "id") UUID id){
+        Optional<ProductModel> product = productRepository.findById(id);
+        if(product.isEmpty()){
+           return ResponseEntity.status(HttpStatus.NOT_FOUND).body("product not found");
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(product.get());
+    }
+
+
+    @PutMapping("/products/{id}")
+    public ResponseEntity<Object> updateProduct(@PathVariable(value = "id") UUID id,
+                                               @RequestBody @Valid ProductRecordDto productRecordDto) {
+        Optional<ProductModel> product = productRepository.findById(id);
+        if (product.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("product not found");
+        }
+        ProductModel productModel = product.get();
+        BeanUtils.copyProperties(productRecordDto , productModel);
+
+        ProductModel updateProduct = productRepository.save(productModel);
+        return ResponseEntity.status(HttpStatus.OK).body(updateProduct);
+    }
+
+    @DeleteMapping("/products/{id}")
+    public ResponseEntity<Object> deleteProduct(@PathVariable(value = "id") UUID id){
+        Optional<ProductModel> product = productRepository.findById(id);
+        if(product.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("product not found");
+        }
+        productRepository.delete(product.get());
+        return ResponseEntity.status(HttpStatus.OK).body("product deleted sucessfully.");
+
+    }
 }
